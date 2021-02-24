ab8e9839f3f8d3badd8d61432e60782b12cce830
Justine Boyer <juboyer@student.wethinkcode.co.za>
2021-02-24 10:25:01 +0200

    user component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
-import UserItem from "./components/users/UserItem"
+import Users from "./components/users/Users"
 
 class App extends Component {
 
@@ -10,7 +10,9 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar />
-        <UserItem />
+        <div className="container">
+          <Users />
+        </div>
       </div>
     );
   }
