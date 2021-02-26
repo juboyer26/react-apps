544a0b44a81fbb5cf516b3ee41e90ecf7bb437bb
Justine Boyer <juboyer@student.wethinkcode.co.za>
2021-02-26 15:52:52 +0200

    final changes for now

--- monesters-rolodex/src/App.js
+++ monesters-rolodex/src/App.js
@@ -19,23 +19,21 @@ class App extends Component {
     .then(users => this.setState({monsters: users}))
   }
 
+  handleChange = e => {
+    this.setState({searchField: e.target.value})
+  }
+
   render() {
     const { monsters, searchField } = this.state;
-    // const monster = this.state.monsters;
-    // const searchField = this.state.searchField;
 
     const filterMonsters = monsters.filter(monster =>
       monster.name.toLowerCase().includes(searchField.toLowerCase()))
+
     return ( 
     <div className = "App" >
-      
+      <h1>Monster Rolodex</h1>
       < SearchBox placeholder = "search"
-      handleChange = {
-          e =>
-          this.setState({
-              searchField: e.target.value
-            })}
-            />
+      handleChange = {this.handleChange} />
       < CardList monsters={filterMonsters} />
       
     </div>
